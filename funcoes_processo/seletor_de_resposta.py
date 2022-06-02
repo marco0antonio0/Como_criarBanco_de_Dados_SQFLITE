fad858baf478b3bb4a30906872fbb7628c07d0ab
marco0antonio0 <marcomesquitajr@hotmail.com>
2022-06-02 17:14:01 -0300

    master

--- funcoes_processo/seletor_de_resposta.py
+++ funcoes_processo/seletor_de_resposta.py
@@ -1,8 +1,9 @@
 # etapasContatos[id][nome,etapa]
 # etapasContatos[id][ 0,  1 ]
-status = True
+
 etapasContatos = []
 def seletor_de_eventos(nome_contato,lista_mensagens):
+  status = True
   #============================================
   #         Varrer Lista
   for i in range(0,len(etapasContatos)): 
@@ -14,29 +15,28 @@ def seletor_de_eventos(nome_contato,lista_mensagens):
       if 1 == etapasContatos[i][1]:
         etapa = etapasContatos[i][1]
         nome = etapasContatos[i][0]
-        print('etapa ',etapa)
-        #status = False
+        print(etapasContatos)
+        status = False
         break
       #========================================
       #     Etapa 2
       if 2 == etapasContatos[i][1]:
         etapa = etapasContatos[i][1]
         nome = etapasContatos[i][0]
-        print('etapa ',etapa)
-        #status = False
+        print(etapasContatos)
+        status = False
         break
       #========================================
       #     Etapa 3
       if 3 == etapasContatos[i][1]:
         etapa = etapasContatos[i][1]
         nome = etapasContatos[i][0]
-        print('etapa ',etapa)
-        #status = False
+        print(etapasContatos)
+        status = False
         break
   if status:
     nome = nome_contato
     etapa = 1
     etapasContatos.append([nome,etapa])
-    print('etapa ',etapa)
     print(etapasContatos)
   
